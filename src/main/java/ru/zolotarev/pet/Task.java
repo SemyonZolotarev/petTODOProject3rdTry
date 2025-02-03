97825ccad882d61f3e3cbc74fb91b165f21752c9
Semyon Zolotarev <dwzf2aem@mail.ru>
2025-02-03 23:05:16 +0300

    3rd try

--- src/main/java/ru/zolotarev/pet/Task.java
+++ src/main/java/ru/zolotarev/pet/Task.java
@@ -1,15 +1,28 @@
 package ru.zolotarev.pet;
 
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
-@Data
+@AllArgsConstructor
+@Setter
+@Getter
 public class Task {
     private static int count = 0;
-    private final int id = ++count;
-    private String taskName;
+    private final int ID = ++count;
+    private String name;
     private String description;
     private LocalDate deadline;
     private TaskStatus status = TaskStatus.TODO;
+
+    public String toString() {
+        return "------------" +
+                "TaskName is '" + this.getName() + "' .ID: " + this.getID()
+                + ".\n Status: " + this.getStatus()
+                + ".\n Deadline: " + this.getDeadline()
+                + ".\n Description: " + this.getDescription() +
+                "------------";
+    }
 }
